a2d2b98fe07cf1fdaa9c940961f746fa8f3f92d2
Nathan Kerr <40112154@ads.qub.ac.uk>
2019-10-08 22:48:09 +0100

    Connection change from local db to hosted db -- Nathan Kerr

--- ATM/Connections.cs
+++ ATM/Connections.cs
@@ -11,16 +11,16 @@ namespace ATM
         public static SqlConnection con = null;
 
         //hosted connection
-        //public void sql_connection()
-        //{
-        //    con = new SqlConnection(@"Data Source=smartcloud.cuawljtgnw6p.eu-west-2.rds.amazonaws.com,1433;Initial Catalog=ATM;User ID=NathanKerr;Password=SmartCloud10;MultipleActiveResultSets=True");
-        //}
-
-        //local database
         public void sql_connection()
         {
-            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\ATM.mdf;Integrated Security=True;MultipleActiveResultSets=True");
+            con = new SqlConnection(@"Data Source=smartcloud.cuawljtgnw6p.eu-west-2.rds.amazonaws.com,1433;Initial Catalog=ATM;User ID=NathanKerr;Password=SmartCloud10;MultipleActiveResultSets=True");
         }
+
+        //local database
+        //public void sql_connection()
+        //{
+        //    con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\ATM.mdf;Integrated Security=True;MultipleActiveResultSets=True");
+        //}
 
     }
 }
